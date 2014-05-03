5892c7e371c7f02d49a7a8e98a416b90e8375fc8
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-04 09:43:47 +1000

    functions have beeen updated

--- R/functions.R
+++ R/functions.R
@@ -105,4 +105,152 @@ cmbn.fls <- function(file){
   return(pr.cst)
 }
 
+##########################
+# Create a summary table #
+##########################
+CreateTable <- function(dataset, fac){
+  a <- dataset[c("date", fac, "value")] #extract required columns
+  colnames(a) <- c("date","variable","value") #change column names for cast
+  means <- cast(a, date~variable, mean, na.rm = TRUE) 
+  ses <- cast(a,date~variable,function(x) ci(x,na.rm=TRUE)[4])
+  colnames(ses)[2:ncol(ses)] <- paste(colnames(ses)[2:ncol(ses)],"SE",sep=".")
+  samples <- cast(a,date~variable,function(x) sum(!is.na(x))) #sample size
+  colnames(samples)[2:ncol(samples)] <- paste(colnames(samples)[2:ncol(samples)],"N",sep=".")
+  mer <- Reduce(function(...) merge(..., by = "date"), list(means, ses, samples)) #merge datasets
+  mer <- mer[,c(1, order(names(mer)[-grep("date|N", names(mer))])+1, grep("N", names(mer)))] #re-order columns
+  mer$date <- as.character(mer$date) # date is turned into character for knitr output 
+  return(mer)
+}
+
+#function which creates excel worksheets
+crSheet <- function(sheetname, dataset){
+  #create sheet
+  sheet <- createSheet(wb, sheetName = sheetname)
+  
+  #add data to the sheet
+  addDataFrame(dataset, sheet, showNA = TRUE, row.names = FALSE, startRow = 2)
+  
+  #title of the sheet
+  addDataFrame(t(c(sheetname, "unit=mg DrySoil(kg)^(-1) day^(-1)")), sheet, startRow = 1, row.names = FALSE, col.names = FALSE)
+}
+
+############################
+# make a summary dataframe #
+############################
+Crt_SmryDF <- function(data, val = "value"){
+  x <- data[ ,val]
+  Mean <- mean(x, na.rm = TRUE)
+  SE <- ci(x, na.rm = TRUE)[[4]]
+  N  <- sum(!is.na(x))
+  data.frame(Mean, SE, N)
+}
+
+####################
+# plot mean and se #
+####################
+PltMean <- function(data){
+  vars <- c(substitute(nutrients),
+            substitute(NO[3]^"-"-N), 
+            substitute(NH[4]^"+"-N),
+            substitute(PO[4]^"3-"-P))
+  # subsitute returens argument as it is without calculation (similar to expression())
+  
+  yvars <- lapply(vars, function(x) bquote(Soil-extractable~.(x)))
+  # bquote allows one to call an object and return expression
+  
+  ylabs <- lapply(yvars, function(x) {
+    c(expression(), 
+      bquote(atop(paste(.(x)), paste((mg~DS_kg^-1)))))         
+  })
+  
+  # atop: put the 1st argument on top of the 2nd
+  
+  ylab <- ifelse(length(unique(data$variable)) > 1, ylabs[[1]],
+                 ifelse(unique(data$variable) == "no", ylabs[[2]], 
+                        ifelse(unique(data$variable) == "nh", ylabs[[3]],
+                               ylabs[[4]])))
+  
+  colfactor <- ifelse(any(names(data) == "chamber"), "chamber", "temp")
+  
+  p <- ggplot(data, aes_string(x = "date", y = "Mean", col = colfactor))
+  
+  p2 <- p + geom_line(size = 1) + 
+    geom_errorbar(aes_string(ymin = "Mean - SE", ymax = "Mean + SE", col = colfactor), width = 5) + 
+    labs(x = "Time", y = ylab)
+  
+  # change colors, linetype and associated legend according to plotting groups (chamber or treatment)
+  if(colfactor == "temp") p3  <- p2 +  
+    scale_color_manual(values = c("blue", "red"), "Temp trt", labels = c("Ambient", "eTemp")) else
+      p3 <- p2 + 
+    scale_color_manual(values = palette(), "Chamber", labels = paste("Ch", c(1:12), sep = "_")) +
+    scale_linetype_manual(values = rep(c("solid", "dashed"), 6), "Chamber", labels = paste("Chamber", c(1:12), sep = "_")) +
+    guides(color = guide_legend(keyheight = 0.7))
+  
+  # add asterisk on P graphs at temperature treatments
+  if(colfactor == "chamber" | !any(unique(data$variable) == "po")) p3 else{
+    newDF <- subset(data, time %in% c(2, 6)) # the times where "*" is placed
+    ant_pos <- ddply(newDF, .(date, variable), summarise, Mean = max(Mean + SE)) #y position of "*"
+    ant_pos <- subset(ant_pos, variable == "po") # only applied to PO data
+    ant_pos$lab <- "*"
+    ant_pos$temp <- factor("amb", levels=c("amb", "elve")) 
+    # the original data frame uses "temp", so it needs to have "temp" as well in ggplot2
+    # but it doesn't really do anything    
+    p3 +  geom_text(data = ant_pos, aes(x =date, y = Mean, label= lab), col = "black", vjust = 0)
+  }
+}
+
+##############################
+# Save ggplot in PDF and PNG #
+##############################
+ggsavePP <- function(filename, plot, width, height){
+  ggsave(filename = paste(filename, ".pdf", sep = ""), 
+         plot = plot, 
+         width = width, 
+         height = height)
+  
+  ggsave(filename = paste(filename, ".png", sep = ""), 
+         plot = plot, 
+         width = width, 
+         height = height, 
+         dpi = 600)
+}
+
+#############################################
+# compare different auto-correlation models #
+#############################################
+
+atcr.cmpr <- function(model, rndmFac){
+  if(rndmFac == "chamber/location"){
+    model2 <- update(model,corr=corCompSymm(form=~1|chamber/location)) 
+  } else {
+    if(rndmFac == "chamber"){
+      model2 <- update(model,corr=corCompSymm(form=~1|chamber))
+    } else {
+      model2 <- update(model,corr=corCompSymm(form=~1|id))
+    }
+  }
+  
+  model3 <- update(model,correlation=corARMA(q=2))
+  model4 <- update(model,correlation=corAR1()) 
+  model5 <- update(model,correlation=corARMA(q=1))
+  a <- anova(model,model2,model3,model4,model5)
+  rownames(a) <- c("NULL", "corCompSymm", "corARMA(q=2)", "corAR1()", "corARMA(q=1)")
+  models <- list(model, model2, model3, model4, model5, 'models' = a)
+  return(models)
+}
+
+###########################################
+# produce box plots with transformed data #
+###########################################
+# log OR sqrt OR power(1/3) OR inverse
+bxplts <- function(value, ofst = 0, data){
+  par(mfrow = c(2, 3))
+  y <- data[[value]] + ofst #ofst is added to make y >0
+  boxplot(y ~ temp*time, data)
+  boxplot(log(y) ~ temp*time, main = "log", data)
+  boxplot(sqrt(y) ~ temp*time, main = "sqrt", data)
+  boxplot(y^(1/3) ~ temp*time, main = "power(1/3)", data)
+  boxplot(1/y ~ temp*time, main = "inverse", data)
+  par(mfrow = c(1,1))
+}
 
