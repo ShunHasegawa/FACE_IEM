9f65110b66064a0c22698b341dd838bdf852a37a
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-04 09:28:32 +1000

    moved to new folder

---
+++ R/functions.R
@@ -0,0 +1,95 @@
+#######################
+#model simplification #
+#######################
+ana<-function(model){
+  mod2<-update(model,method="ML") #change method from REML to ML
+  stai<-stepAIC(mod2,trace=FALSE) #model simplification by AIC
+  dr<-drop1(stai,test="Chisq") #test if removing a factor even more significantly lowers model
+  model<-update(stai,method="REML")
+  ifelse(all(dr[[4]]<0.05,na.rm=TRUE),anr<-anova(model),anr<-NA) 
+  #dr[[4]]<0.05-->unable to remove any more factors so finlize the results by changsing the method back to REML
+  return(list(step.aic=stai$anova,drop1=dr,anova.reml=anr,model.reml=model,model.ml=stai))
+}
+
+###################################
+# Coorect AQ2 result based on CCV #
+###################################
+
+# subset dataset beween each ccv
+Crrct.ccv <- function(x, data, ccval = 7.5){
+  # subset dataset between given ccvs
+  ccv.ps <- grep("V$", as.character(data$Sample.ID))
+  
+  ts <- data[c(ccv.ps[x] : ccv.ps[x + 1]),]
+  trng <- range(ts$times)
+  xv <- as.numeric(trng)
+  yv <- ts$Result[ts$times == trng[1] | ts$times == trng[2]] 
+  
+  b <- ccval * (1 / yv[1] - 1 / yv[2]) / (xv[1] - xv[2])
+  a <- ccval / yv[1] - b * xv[1]
+  
+  ts$Result <- ts$Result * (a + b * as.numeric(ts$times))
+  ts$times <- NULL
+  return(ts)
+}
+
+# applied the function above for each subsets of ccvs
+Crrtct.ccv.df <- function(data, ccval = 7.5){
+  # make time factor as numeric
+  a <- sapply(as.character(data$Time), strsplit, " ")
+  b <- ldply(a, function(x) paste(x[c(5, 2, 3, 4)], collapse = "/"))
+  
+  b$V1 <- ymd_hms(b$V1)
+  
+  names(b) <- c("Time", "times")
+  
+  # merge
+  mrg.res <- merge(data, b, by = "Time")
+  
+  # reorder accoding to time
+  mrg.res <- mrg.res[order(mrg.res$times), ]
+  
+  ccv.ps <- grep("V$", as.character(mrg.res$Sample.ID))
+  
+  # re-caluculate the results
+  a <- ldply(1:(length(ccv.ps)-1), function(x) Crrct.ccv(x, data = mrg.res, ccval))
+  
+  return(a)
+}
+
+######################################################
+# process and combine aq 2 data, then create a table #
+######################################################
+prcsAQ2 <- function(data){
+  # remove ccv, ccb, standard
+  res <- data[-grep("^C|^STANDARD", as.character(data$Sample.ID)),]
+  
+  # remove dup, top, middle
+  res <- res[-grep("dup$|top|middle", as.character(res$Sample.Details)),]
+  
+  # sample labels
+  a <- strsplit(as.character(res$Sample.Details), "[.]")
+  
+  # turn this into data frame
+  a.df <- ldply(a)
+  names(a.df)[c(1, 4, 5)] <- c("Date", "ring", "plot")
+  a.df$Date <- ymd(a.df$Date)
+  res.df <- cbind(a.df, res)
+  res.df <- res.df[c("Date", "ring", "plot", "Result")]
+  return(res.df)
+}
+
+cmbn.fls <- function(file){
+  # read files
+  rd.fls <- lapply(file, function(x) read.csv(paste("Data/AQ2/ReadyToProcess/", x, sep = ""), header = TRUE))
+  
+  # process and make data frame for each test type
+  pr.df <- ldply(rd.fls, function(x) ddply(x, .(Test.Name), prcsAQ2))
+  
+  # reshape
+  names(pr.df)[5] <- "value"
+  pr.cst <- cast(pr.df, Date + ring + plot ~ Test.Name)
+  return(pr.cst)
+}
+
+
