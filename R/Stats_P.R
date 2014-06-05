98cebb9a0c988d4b1c2f4408c626535e1316f513
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-06-06 08:16:10 +1000

    fit soil variables

--- R/Stats_P.R
+++ R/Stats_P.R
@@ -56,7 +56,6 @@ qqline(residuals.lm(Fml_pre))
 ############
 # post-co2 #
 ############
-
 bxplts(value= "p", data= subsetD(iem, post))
 bxcxplts(value= "p", data= subsetD(iem, post), sval = 0.9, fval = 2)
 # adding constant value of 1.56 may improve
@@ -109,6 +108,110 @@ qqnorm(Fml_post, ~ resid(.)|id)
 qqnorm(residuals.lm(Fml_post))
 qqline(residuals.lm(Fml_post))
 
+# fit soil variable
+library(lattice)
+print(xyplot((p + 1.6)^(-1.1515) ~ Moist | ring + plot, subsetD(iem, !pre), type = c("r", "p")))
+print(xyplot((p + 1.6)^(-1.1515) ~ Temp_Mean | ring + plot, subsetD(iem, !pre), type = c("r", "p")))
+print(xyplot((p + 1.6)^(-1.1515) ~ Temp_Min | ring + plot, subsetD(iem, !pre), type = c("r", "p")))
+print(xyplot((p + 1.6)^(-1.1515) ~ Temp_Max | ring + plot, subsetD(iem, !pre), type = c("r", "p")))
+
+
+print(xyplot((p + 1.6)^(-1.1515) ~ Moist | co2, subsetD(iem, !pre), type = c("r", "p"), group = id))
+print(xyplot((p + 1.6)^(-1.1515) ~ Temp_Mean | co2, subsetD(iem, !pre), type = c("r", "p"), group = id))
+print(xyplot((p + 1.6)^(-1.1515) ~ Temp_Min | co2, subsetD(iem, !pre), type = c("r", "p"), group = id))
+print(xyplot((p + 1.6)^(-1.1515) ~ Temp_Max | co2, subsetD(iem, !pre), type = c("r", "p"), group = id))
+
+?xyplot
+print(xyplot((p + 1.6)^(-1.1515) ~ Moist | ring, subsetD(iem, !pre), type = c("r", "p")))
+print(xyplot((p + 1.6)^(-1.1515) ~ Temp_Mean | ring, subsetD(iem, !pre), type = c("r", "p")))
+print(xyplot((p + 1.6)^(-1.1515) ~ Temp_Min | ring, subsetD(iem, !pre), type = c("r", "p")))
+print(xyplot((p + 1.6)^(-1.1515) ~ Temp_Max | ring, subsetD(iem, !pre), type = c("r", "p")))
+
+
+print(xyplot((p + 1.6)^(-1.1515) ~ Moist | co2, subsetD(iem, !pre), type = c("r", "p")))
+print(xyplot((p + 1.6)^(-1.1515) ~ Temp_Mean | co2, subsetD(iem, !pre), type = c("r", "p")))
+print(xyplot((p + 1.6)^(-1.1515) ~ Temp_Min | co2, subsetD(iem, !pre), type = c("r", "p")))
+print(xyplot((p + 1.6)^(-1.1515) ~ Temp_Max | co2, subsetD(iem, !pre), type = c("r", "p")))
+
+
+
+m1 <- lme((p + 1.6)^(-1.1515) ~ co2 * time + Moist + Temp_Mean + Temp_Min + Temp_Max, 
+          random = ~1|ring/plot,  data = subsetD(iem, post))
+m2 <- lme((p + 1.6)^(-1.1515) ~ co2 * time + Temp_Min, 
+          random = ~1|ring/plot,  data = subsetD(iem, post))
+
+m1 <- lme((p + 1.6)^(-1.1515) ~ co2*(Moist + Temp_Mean + Temp_Min + Temp_Max), 
+          random = ~1|ring/plot,  data = subsetD(iem, !pre), method = "ML")
+Anova(m8)
+
+m2 <- lme((p + 1.6)^(-1.1515) ~ co2*(Moist + Temp_Mean), 
+          random = ~1|ring/plot,  data = subsetD(iem, !pre))
+m3 <- lme((p + 1.6)^(-1.1515) ~ co2 + Moist + Temp_Mean + Temp_Min + Temp_Max, 
+          random = ~1|ring/plot,  data = subsetD(iem, !pre))
+m4 <- lme((p + 1.6)^(-1.1515) ~ co2 + Moist + Temp_Mean + Temp_Min, 
+          random = ~1|ring/plot,  data = subsetD(iem, !pre))
+m5 <- lme((p + 1.6)^(-1.1515) ~ co2*(Moist + Temp_Min), 
+          random = ~1|ring/plot,  data = subsetD(iem, !pre))
+m6 <- lme((p + 1.6)^(-1.1515) ~ co2*(Moist + Temp_Min + Temp_Max + Temp_Mean) + Temp_Min:Temp_Max, 
+          random = ~1|ring/plot,  data = subsetD(iem, !pre), , method = "ML")
+m7 <- lme((p + 1.6)^(-1.1515) ~ co2 * Moist * Temp_Min * Temp_Max * Temp_Mean, 
+          random = ~1|ring/plot,  data = subsetD(iem, !pre), method = "ML")
+m7 <- lme((p + 1.6)^(-1.1515) ~ (co2 + Moist + Temp_Min + Temp_Max + Temp_Mean)^4, 
+          random = ~1|ring/plot,  data = subsetD(iem, !pre), method = "ML")
+m7.1 <- lme((p + 1.6)^(-1.1515) ~ (co2 + Moist + Temp_Min + Temp_Max + Temp_Mean)^2, 
+          random = ~1|ring/plot,  data = subsetD(iem, !pre), method = "ML")
+
+
+m8 <- stepAIC(m7.1)
+Anova(m8)
+m7.2 <- update(m8, ~. - Moist:Temp_Min)
+anova(m8, m7.2)
+m9 <- stepAIC(m7.2)
+Anova(m9)
+summary(m9)
+AIC(m9)
+
+qqnorm(m1, ~ resid(.)|id)
+qqnorm(residuals.lm(m1))
+qqline(residuals.lm(m1))
+
+AIC(m1)
+
+m9 <- update(m8, method = "REML")
+Anova(m9)
+AIC(m9)
+AIC(m1)
+
+Anova(m8)
+
+Anova(m6)
+
+anova(m1, m6)
+summary(m1)
+plot(allEffects(m8))
+
+boxplot(Moist ~ co2 * time, data = iem)
+Anova(m1)
+
+head(iem)
+SoilVarDF <- iem[, c("co2", "time", "Moist", "Temp_Mean", "Temp_Min", "Temp_Max")]
+SoilVarDF_CO2 <- ddply(SoilVarDF, .(time, co2),
+                       function(x) colMeans(x[c("Moist", "Temp_Mean", "Temp_Min", "Temp_Max")],na.rm = TRUE))
+SoilVarMlt <- melt(SoilVarDF_CO2, id = c("co2", "time"))
+SoilVarMlt$type <- factor(ifelse(SoilVarMlt$variable != "Moist", "Temp", "Moist"))
+head(SoilVarMlt)
+
+theme_set(theme_bw())
+p <- ggplot(SoilVarMlt, aes(x = time, y = value, shape = variable, col = co2))
+pl <- p + geom_point() +
+  facet_grid(type ~., scale = "free_y")
+ggsavePP(filename = "output//figs/FACE_IEM_SoilVarSummary", plot = pl, width = 6, height = 4)
+
+plot(allEffects(m1))
+summary(m1)
+qqnorm(m1, ~ resid(.)|id)
+qqnorm(residuals.lm(m1))
+qqline(residuals.lm(m1))
 
 ## ---- Stat_FACE_IEM_Phosphate_preCO2_Smmry
 # The starting model is:
