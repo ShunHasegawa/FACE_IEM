c7b48c781b04afee85a3c8a839c6109c81d74991
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-06-13 20:16:54 +1000

    only blocking analysis is left

--- R/Stats_P.R
+++ R/Stats_P.R
@@ -153,6 +153,9 @@ print(xyplot((p + 1.6)^(-1.1515) ~ Temp_Max | ring + plot, subsetD(iem, !pre), t
 ############
 # Blocking #
 ############
+# Figs from above showed similarrity in Soil Moist and Temp_Max
+# between Ring 1&2, 3&4, and 5&6 so block them
+
 iem$block  <- recode(iem$ring, "c(1,2) = 'A'; c(3,4) = 'B'; c(5,6) = 'C'")
 
 # Note Temp_Max and log(Moist) appears to be correlated so shouln't be 
