f3ae5206b541454eb244f7bb1b4fbc2e40d9d6a5
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-06-20 13:08:04 +1000

    size and color don't change with IEM-P anaymore

--- R/Stats.R
+++ R/Stats.R
@@ -2,17 +2,14 @@
 # Moist and Temp #
 ##################
 theme_set(theme_bw())
-iem$TrP <- (iem$p + 1.6)^(-1.1515)
 
-p <- ggplot(subsetD(iem, !pre), aes(x = Temp_Max, y = log(Moist), size = TrP, col = TrP))
-p2 <- p + geom_point(alpha = .5) + 
-  scale_size(range = c(8, 1)) +
-  scale_color_gradientn(colours = c("red", "yellow", "blue"))
+p <- ggplot(subsetD(iem, !pre), aes(x = Temp_Max, y = log(Moist), col = ring))
+p2 <- p + geom_point(alpha = .5) 
 
 pl  <- p2 + facet_wrap( ~ ring)
-ggsavePP(file = "output/figs/FACE_IEM_P_withSoilVar_ring", plot = pl, width = 6, height = 6)
+ggsavePP(file = "output/figs/FACE_IEM_SoilVa_Ringr", plot = pl, width = 6, height = 6)
 
-ggsavePP(file = "output/figs/FACE_IEM_P_withSoilVar", plot = p2, width = 6, height = 6)
+ggsavePP(file = "output/figs/FACE_IEM_SoilVa", plot = p2, width = 6, height = 6)
 
 # Soil moisture and temperature seems to be correlated
 # Temperature may regulate soil moisture
