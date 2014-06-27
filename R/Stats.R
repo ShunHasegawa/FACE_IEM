d74deaee06a8abd0240f8dc8ef940aaa93e15ad1
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-06-27 10:03:15 +1000

    fig of moist against temp is moved to fig.R

--- R/Stats.R
+++ R/Stats.R
@@ -1,19 +1,3 @@
-##################
-# Moist and Temp #
-##################
-theme_set(theme_bw())
-
-p <- ggplot(subsetD(iem, !pre), aes(x = Temp_Max, y = log(Moist), col = ring))
-p2 <- p + geom_point(alpha = .5) 
-
-pl  <- p2 + facet_wrap( ~ ring)
-ggsavePP(file = "output/figs/FACE_IEM_SoilVa_Ringr", plot = pl, width = 6, height = 6)
-
-ggsavePP(file = "output/figs/FACE_IEM_SoilVa", plot = p2, width = 6, height = 6)
-
-# Soil moisture and temperature seems to be correlated
-# Temperature may regulate. 
-
 ###########
 # Nitrate #
 ###########
