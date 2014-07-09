d39afc66a08df73e1a8c6d278e7d4c823ff95313
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-07-09 16:45:57 +1000

    raw data line were added

--- R/Figs.R
+++ R/Figs.R
@@ -41,17 +41,47 @@ ggsavePP(filename = "output//figs/FACE_IEM_CO2Trt", plot = pl, width = 6, height
 # plot soil moist and temp for each incubation periods #
 ########################################################
 
+## raw (before taking two week mean) ##
+load("Data/FACE_TDR_ProbeDF.RData")
+
+# subset IEM tdr
+iemTDR <- subsetD(FACE_TDR_ProbeDF, Sample == "IEM")
+
+# add co2 and remove unnecessary columns
+iemTDR <- within(iemTDR, {
+  co2 <- factor(ifelse(ring %in% c(1, 4, 5), "elev", "amb"))
+  plot <- factor(plot)
+  Temp_Min <- NULL
+  Temp_Max <- NULL
+  Sample <- NULL
+})
+names(iemTDR)[5] <- "Temp"
+
+iemTDRdf <- melt(iemTDR, id = c("Date", "co2", "ring", "plot"))
+iemTDRdf$type <- iemTDRdf$variable
+
+iemTDR_RngMean <- ddply(iemTDRdf, .(Date, co2, ring, variable, type), summarise, value = mean(value, na.rm = TRUE))
+iemTDR_co2Mean <- ddply(iemTDR_RngMean, .(Date, co2, variable, type), summarise, value = mean(value, na.rm = TRUE))
+
+## Make figs
 SoilVarDF <- iem[, c("co2", "ring", "date", "Moist", "Temp_Mean", "Temp_Min", "Temp_Max")]
 
-# co2
+## co2 ##
 pl <- PltSoilVar(data = SoilVarDF, var = "co2") +
-  scale_color_manual(values = c("blue", "red"), expression(CO[2]~trt), labels = c("Ambient", expression(eCO[2])))
+  scale_color_manual(values = c("blue", "red"), expression(CO[2]~trt), 
+                     labels = c("Ambient", expression(eCO[2]))) +
+  geom_line(aes(x = Date, y = value, group = co2), data = iemTDR_co2Mean) +
+  geom_vline(xintercept = as.numeric(unique(iem$insertion)), col = "green", size = .5)
+
 ggsavePP(filename = "output//figs/FACE_IEM_SoilVarMonth_CO2", plot = pl, width = 6, height = 4)
 
 
-# ring
+## ring ##
 pl <- PltSoilVar(data = SoilVarDF, var = "ring") +
-  scale_color_manual(values = palette(), "Ring", labels = paste("Ring", c(1:6), sep = "_"))
+  scale_color_manual(values = palette(), "Ring", labels = paste("Ring", c(1:6), sep = "_")) +
+  geom_line(aes(x = Date, y = value, group = ring), data = iemTDR_RngMean) +
+  geom_vline(xintercept = as.numeric(unique(iem$insertion)), col = "green", size = .5)
+
 ggsavePP(filename = "output//figs/FACE_IEM_SoilVarMonth_Ring", plot = pl, width = 6, height = 4)
 
 ############################
