ed3573def5a09228db090d42bcb84f89821b7a9f
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-07-10 16:29:34 +1000

    95 % ci is calculated for estimates

--- R/Stats_P.R
+++ R/Stats_P.R
@@ -206,59 +206,38 @@ qqnorm(resid(Fml_ancv))
 qqline(resid(Fml_ancv))
 
 # confidence interval for estimated parameters
-test <- confint(Fml_ancv, method = "boot")
-test
+CIs <- confint(Fml_ancv, method = "boot")
+CIs <- CIs[c(5:10),]
+coefs <- summary(Fml_ancv)$coefficients
+ciDF <- cbind(CIs, Estimate = coefs[,1])
 
+# calculate actual values
+Est.val <- rbind(
+  int = ciDF[1, ],
+  co2elev = ciDF[2, ] + ciDF[1, 1],
+  Moist = ciDF[3, ],
+  Temp_Mean = ciDF[4, ],
+  co2elev.Moist = ciDF[5, ] + ciDF[3, 1],
+  co2elev.Temp_Mean = ciDF [6, ] + ciDF[4, 1]
+  )
 
-newDF$id <- newDF$ring:newDF$plot
-testPr <- cbind(newDF,predict(mlmer, newdata = newDF))
-names(testPr)[7] <- "predict"
-
-a <- summary(Fml_ancv)
-a$coefficients
-
-
-
-m1 <- lm(log(p) ~ co2 * (Moist + Temp_Mean), data = subsetD(iem, !pre))
-summary(m1)
-anova(m1)
-visreg(m1, "Temp_Mean", by = "co2", overlay = TRUE, trans = exp)
-visreg(m1, "Moist", by = "co2", overlay = TRUE, trans = exp)
-
-plot(log(p) ~ Temp_Mean, subsetD(iem, !pre), col = co2)
-a <- lm(log(p) ~ co2 * Temp_Mean, data = subsetD(iem, !pre))
-cf <- coef(a)
-abline(cf[1], cf[3])
-abline(cf[1] + cf[2], cf[3] + cf[4], col = "red")
-
-par(mfrow = c(1,2))
-plot(p ~ Temp_Mean, subsetD(iem, !pre), col = co2)
-plot(p ~ Moist, subsetD(iem, !pre), col = co2)
-a <- lm(log(p) ~ co2 * Temp_Mean, data = subsetD(iem, !pre))
-cf <- coef(a)
-abline(cf[1], cf[3])
-abline(cf[1] + cf[2], cf[3] + cf[4], col = "red")
-?abline
-
-
-# ring mean
-df <- subsetD(iem, !pre)
-RngM <- ddply(df, .(ring, time, co2), function(x) colMeans(x[, c("p", "Moist", "Temp_Mean")]))
-coM <- ddply(RngM, .(time, co2), function(x) colMeans(x[, c("p", "Moist", "Temp_Mean")]))
-
-p <- ggplot(data =  coM, aes(x = Temp_Mean, y = Moist, size = p, col = co2))
-p + geom_point(alpha = .5) +
-    scale_color_manual(values = c("blue", "red"), expression(CO[2])) +
-  scale_size(range = c(1 , 20), breaks = seq(1, 9, 1))
-range(RngM$p)
-
-## Plot predicted values for each block ##
+########################################
+# Plot predicted values for each block #
+########################################
 
 # Create a data frame for explanatory
+
+# moisture and given temp (median)
 expDF <- with(iem, expand.grid(ring = unique(ring), 
                                plot = unique(plot),
                                Moist = seq(min(Moist, na.rm = TRUE), max(Moist, na.rm = TRUE), length.out= 100),
-                               Temp_Mean = seq(min(Temp_Mean, na.rm = TRUE), max(Temp_Mean, na.rm = TRUE), length.out= 100)))
+                               Temp_Mean = median(Temp_Mean, na.rm = TRUE)))
+
+# temperature and give moist
+# expDF <- with(iem, expand.grid(ring = unique(ring), 
+#                                plot = unique(plot),
+#                                Temp_Mean = seq(min(Temp_Mean, na.rm = TRUE), max(Temp_Mean, na.rm = TRUE), length.out= 100),
+#                                Moist = median(Moist, na.rm = TRUE)))
 expDF <- within(expDF, {
   block = recode(ring, "c(1,2) = 'A'; c(3,4) = 'B'; c(5,6) = 'C'")
   co2 = factor(ifelse(ring %in% c(1, 4, 5), "elev", "amb"))
@@ -267,29 +246,27 @@ expDF <- within(expDF, {
 # adjust moisture range for each block
 boxplot(Moist ~ block, data = expDF)
 
-AdjexpDF <- ldply(list("A", "B", "C"), function(x) BlkminMoist(variable = x, data = expDF))
+sAdjexpDF <- ldply(list("A", "B", "C"), function(x) BlkminMoist(variable = x, data = expDF))
 boxplot(Moist ~ block, data = AdjexpDF)
 
 # predicted values
-PredVal <- predict(Fml_ancv, level = 0:3, newdata = AdjexpDF)
-PredDF <- cbind(AdjexpDF, PredVal[, c(3:7)])
+PredVal <- predict(Fml_ancv, re.form = ~(1|block), newdata = AdjexpDF)
+PredDF <- cbind(AdjexpDF, PredVal)
 
 # plot
 theme_set(theme_bw())
-p <- ggplot(PredDF, aes(x = Moist, y = exp(predict.block), col = co2))
-p + geom_line(group = co2) +
+p <- ggplot(PredDF, aes(x = Moist, y = exp(PredVal), col = co2))
+p + geom_line(aes(group = ring)) +
   geom_point(aes(x = Moist, y = p, col = co2), data = subsetD(iem, !pre)) + 
   scale_color_manual("co2", values = c("blue", "red")) +
-  facet_grid(.~block) +
+  facet_grid(. ~ block)
   labs(y = "IEM-P")
 
 
-p <- ggplot(PredDF, aes(x = log(Moist), y = predict.block, col = co2))
-p + geom_line() +
-  geom_point(aes(x = log(Moist), y = (p + 1.6)^(-1.1515), col = co2), data = subsetD(iem, !pre)) + 
-  scale_color_manual("co2", values = c("blue", "red")) +
-  facet_grid(.~block, scale = "free_x") + 
-  labs(y = expression((p+1.6)^(-1.1515)))
+
+
+
+
 
 
 ## ---- Stat_FACE_IEM_Phosphate_preCO2_Smmry
