c43fd84df75d9625652244deeb01bacb71cd337a
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-07-10 22:44:14 +1000

    new functions are used

--- R/Stats_NH.R
+++ R/Stats_NH.R
@@ -12,30 +12,25 @@ bxplts(value= "nh", data= subsetD(iem, pre))
 nhRmOl <- subset(iem, nh < 800)
 bxplts(value= "nh", data= subsetD(nhRmOl, pre))
 bxplts(value= "nh", ofst= 10, data= subsetD(nhRmOl, pre))
-  # none of the transformation seems to be working well,
-  # but let's carry on with log anyway
+  # none of the transformation seems to be working well, but let's carry on with
+  # log anyway
 
 # different random factor strucures
-m1 <- lme(log(nh + 10) ~ co2 * time, random = ~1|ring/plot, data = subsetD(nhRmOl, pre))
-m2 <- lme(log(nh + 10) ~ co2 * time, random = ~1|ring, data = subsetD(nhRmOl, pre))
-m3 <- lme(log(nh + 10) ~ co2 * time, random = ~1|id, data = subsetD(nhRmOl, pre))
-anova(m1, m2, m3)
-  # m1 is better
+m1 <- lme(log(nh + 10) ~ co2 * time, random = ~1|block/ring/plot, data = subsetD(nhRmOl, pre))
+RndmComp(m1)$anova
+  # m3 is better
+RnMl <- RndmComp(m1)[[3]]
 
 # autocorelation
-atcr.cmpr(m1, rndmFac="ring/plot")$models
-  # model 5 looks better
-
-Iml_pre <- atcr.cmpr(m1, rndmFac="ring/plot")[[5]]
+atcr.cmpr(RnMl)$models
+# model 5 looks better
+Iml_pre <- atcr.cmpr(RnMl)[[5]]
 
 # The starting model is:
 Iml_pre$call
 
 # model simplification
 Anova(Iml_pre)
-
-MdlSmpl(Iml_pre)
-  # time * co2 and co2 are removed
 
 Fml_pre <- MdlSmpl(Iml_pre)$model.reml
 
@@ -65,26 +60,21 @@ bxplts(value= "nh", data= subsetD(iem, post))
   # log seems better
 
 # different random factor strucures
-m1 <- lme(log(nh) ~ co2 * time, random = ~1|ring/plot, data = subsetD(iem, post) )
-m2 <- lme(log(nh) ~ co2 * time, random = ~1|ring, data = subsetD(iem, post) )
-m3 <- lme(log(nh) ~ co2 * time, random = ~1|id, data = subsetD(iem, post) )
-anova(m1, m2, m3)
-  # m2 is better
+m1 <- lme(log(nh) ~ co2 * time, random = ~1|block/ring/plot, data = subsetD(iem, post) )
+RndmComp(m1)$anova
+# m3 is better
+RnMl <- RndmComp(m1)[[3]]
 
 # autocorelation
-atcr.cmpr(m2, rndmFac="ring")$models
-  # model 3 looks better
-
-Iml_post <- atcr.cmpr(m2, rndmFac="ring")[[3]]
+atcr.cmpr(RnMl)$models
+# model 5 looks better
+Iml_post <- atcr.cmpr(RnMl)[[5]]
 
 # The starting model is:
 Iml_post$call
 
 # model simplification
 Anova(Iml_post)
-
-MdlSmpl(Iml_post)
-  # co2 x time, co2 are removed
 
 Fml_post <- MdlSmpl(Iml_post)$model.reml
 
