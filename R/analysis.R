b5b29914fb38a89e925ceecdaef97a77685734bf
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-10-05 17:11:24 +0100

    minor change

--- R/analysis.R
+++ R/analysis.R
@@ -19,8 +19,8 @@ source("R/SummaryExlTable.R")
 # Figs #
 ########
 source("R/Figs.R")
-# Don't worry about the warning message saying containing missing values. This
-# is due to missing soil variable data during 1st incubation period.
+  # Don't worry about the warning message saying containing missing values. This
+  # is due to missing soil variable data during 1st incubation period.
 
 #########
 # Stats #
