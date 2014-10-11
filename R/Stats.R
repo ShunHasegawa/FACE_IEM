9dfb84cb1e9e4fe10c886e226c0707aeac2ae8fd
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2014-10-11 23:54:42 +0100

    three variables are plotted in the same graph

--- R/Stats.R
+++ R/Stats.R
@@ -70,3 +70,120 @@ l_ply(c("Nitrate", "Ammonium", "Phosphate"),
                                 sheetName = x, 
                                 smmaryLst = StatSmmryLst))
 saveWorkbook(wb, "Output/Table/FACE_IEM_Ancv.xlsx")
+
+################################
+# Plot predicted values and CI #
+################################
+
+# list of ancova models
+Lst_ml <- list(no = Fml_ancv_NO, nh = Fml_ancv_NH, p = Fml_ancv_P) 
+
+# bootstrap takes quite long time so apply parallel processing
+
+## set up parallel backend
+
+# registerDoParallel() is normally use to set it up but this time I need to
+# export "BtsCI" and "Fml_ancv" so use the following
+
+cl <- makeCluster(3, type = "SOCK") 
+# three cores will be used as three models will be processed simultaneously as
+# below. 
+
+clusterExport(cl, c("MTdf_temp","BtsCI", "Lst_ml"))
+# exporting objects in the global environment
+
+registerDoSNOW(cl)
+getDoParWorkers()
+
+system.time(
+  ciDF_vsTemp <- ldply(1:3, 
+                    function(x) BtsCI(model = Lst_ml[[x]], 
+                                      MoistVal = MTdf_temp$MoistVal, 
+                                      TempVal = MTdf_temp$TempVal, 
+                                      variable = names(Lst_ml[x])),
+                    .parallel = TRUE,
+                    .paropts = list(.export = c("MTdf_temp","BtsCI", "Lst_ml"),
+                                    .packages = "lme4")
+                    )
+)
+
+# add Moist level
+ciDF_vsTemp$MoistLev <- factor(ciDF_vsTemp$Moist, 
+                               labels = c("Dry", "Moderately wet", "Wet")) 
+
+# save
+save(ciDF_vsTemp, file = "output//data/ciDF_vsTemp.RData") 
+
+#############################
+# conditioning scatter plot #
+#############################
+# need to create multiple graphs on one graphic area with ggplot2
+
+## scatterplot of x and y variables (nutrient against temp at given moisture)
+
+# melt postDF to plot them all together with facet_grid
+postDF_Mlt <- melt(postDF, 
+                   id = names(postDF)[which(!(names(postDF) %in% c("no", "nh", "p")))])
+
+# labels for facet_grid
+ylabs <- c(expression(NO[3]^"-"),expression(NH[4]^"+"), expression(PO[4]^"3-"))
+
+levels(ciDF_vsTemp$variable) <- ylabs
+levels(postDF_Mlt$variable) <- ylabs
+
+MoistLabs <- c(expression(Dry),
+               expression(Moderately~wet),
+               expression(Wet))
+
+levels(ciDF_vsTemp$MoistLev) <- MoistLabs
+levels(postDF_Mlt$MoistLev) <- MoistLabs
+
+scatter <- ggplot(ciDF_vsTemp, 
+                  aes(x = Temp_Mean, y = PredVal, col = co2, fill = co2, group = co2)) +
+  geom_line() +
+  geom_ribbon(aes(ymin = lci, ymax = uci), alpha = .4, color = NA) +
+  # color = NA removes the ribbon edge
+  geom_point(data = postDF_Mlt, aes(x = Temp_Mean, y = log(value)), 
+             alpha = .6, size = 1) +
+  scale_color_manual(values = c("blue", "red"), 
+                     labels =c("Ambient", expression(eCO[2]))) +
+  scale_fill_manual(values = c("blue", "red"), 
+                    labels = c("Ambient", expression(eCO[2]))) +
+  scale_x_continuous(breaks = pretty(ciDF_vsTemp$Temp_Mean)) +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position = c(.12, .96), 
+        legend.title = element_blank(),
+        legend.key.size = unit(.2, "inch"),
+        legend.background = element_rect(fill = alpha('white', 0)),
+        axis.title = element_text(face = "plain")) +
+  facet_grid(variable ~ MoistLev, scales = "free_y", labeller = label_parsed) +
+  labs(x = expression(Soil~temperature~(degree * C)), 
+       y = expression(log(IEM*-adsorbed~nutrients~(ng~cm^"-2"~d^"-1"))))
+
+# moisture range
+M <- with(postDF, seq(min(Moist), max(Moist), length.out = 4))
+MoistDF <- data.frame(x = c(1:3), ymin = M[1:3] * 100, ymax = M[2:4] * 100)
+
+theme_set(theme_bw()) # set plot back ground as white
+
+MoistPlt <- ggplot(MoistDF, 
+                   aes(xmin = x - 0.3, xmax = x + 0.3, ymin = ymin, ymax = ymax)) +
+  geom_rect(fill = "gray30") +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position = "none", 
+        axis.ticks.y = element_blank(),
+        axis.text.y = element_blank()) +
+  coord_flip() +
+  labs(x = "", y = "Given soil moisture (%)")
+
+# merge the plots
+pl <- arrangeGrob(MoistPlt, scatter, ncol = 1, nrow = 2, 
+                  heights = unit(c(1.5, 5), "inches"))
+# grid.arrange creates graphs directly on the device, while arrangeGrob makes 
+# ggplot object which can be save using ggsave. but text font looks bold for
+# some reasons..
+
+ggsavePP(plot = pl, filename = "output//figs/FACE_manuscript/FACE_Pred_IEM_Temp", 
+         width = 6, height = 8)
