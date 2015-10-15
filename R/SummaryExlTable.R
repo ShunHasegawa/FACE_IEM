1d45f8d14cf41f12a1e7166de1efbaaf2050fd2d
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-10-15 13:49:25 +0100

    outlier for Nitrate is removed

--- R/SummaryExlTable.R
+++ R/SummaryExlTable.R
@@ -1,17 +1,21 @@
 # remove outlier
 iemRmOl <- iem
 
-# P
+# Phosphate
 boxplot(iem$p)
 iemRmOl$p[which(iem$p == max(iem$p))] <- NA
 
-# nh
+# Ammonium
 boxplot(iem$nh)
 iemRmOl$nh[which(iem$nh > 800)] <- NA
 
+# Nitrate
+boxplot(log(iem$no))
+iemRmOl$no[which(iem$no == min(iem$no))] <- NA
+
 # NP
 # remove the values calculated from the above values
-iemRmOl$logNP[is.na(iemRmOl$nh)|is.na(iemRmOl$p)] <- NA
+iemRmOl$logNP[is.na(iemRmOl$nh)|is.na(iemRmOl$p)|is.na(iemRmOl$no)] <- NA
 
 # melt dataset
 iemMlt <- melt(iemRmOl, id = names(iem)[which(!(names(iem) %in% c("no", "nh", "p", "logNP")))])
@@ -31,12 +35,16 @@ wb <- createWorkbook()
 sheet <- createSheet(wb,sheetName="raw_data")
 addDataFrame(iem, sheet, showNA=TRUE, row.names=FALSE, characterNA="NA")
 
+# worksheet without outlier
+sheet <- createSheet(wb,sheetName="raw_data_withoutOutlier")
+addDataFrame(iemRmOl, sheet, showNA=TRUE, row.names=FALSE, characterNA="NA")
+
 # worksheets for ring summary
-shnames <- paste("Ring_mean.",c("Nitrate", "Ammonium","Phosphate", "NPRatio", sep=""))
+shnames <- paste("Ring_mean.",c("Nitrate", "Ammonium","Phosphate", "logNPRatio", sep=""))
 l_ply(1:4, function(x) crSheet(sheetname = shnames[x], dataset = RngSmmryTbl[[x]]))
 
 # worksheets for co2 trt summary
-shnames <- paste("CO2_mean.", c("Nitrate", "Ammonium","Phosphate", "NPRatio"), sep = "")
+shnames <- paste("CO2_mean.", c("Nitrate", "Ammonium","Phosphate", "logNPRatio"), sep = "")
 l_ply(1:4, function(x) crSheet(sheetname = shnames[x], dataset = TrtSmmryTbl[[x]]))
 
 #save file
