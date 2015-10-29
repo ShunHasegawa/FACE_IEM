b36372f7ef55b853c0013a0569bdfbf043ab32d6
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-10-29 11:14:24 +0000

    version problems..?

--- R/pckg.R
+++ R/pckg.R
@@ -16,14 +16,23 @@ library(xtable)
 library(visreg)
 library(lmerTest)
 library(gridExtra)
-library(foreach)
-library(doParallel) # set up parallel backend for foreach
-library(snow) # require to difine makeCluster for foreach
-library(doSNOW)
+# library(foreach)
+# library(doParallel) # set up parallel backend for foreach
+# library(snow) # require to difine makeCluster for foreach
+# library(doSNOW)
 library(grid)
 library(quantmod) # compute % change
 library(boot)
 
-install.packages("devtools")
-require(devtools)
-install_github("hadley/ggplot2") 
+# install.packages("devtools")
+# require(devtools)
+devtools::install_github("hadley/ggplot2") 
+# devtools::install_github("jrnold/ggthemes")
+# # library(ggthemes)
+# install.packages("https://cran.r-project.org/src/contrib/Archive/ggplot2/ggplot2_1.0.0.tar.gz",
+#                  repos=NULL, type="source")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/gridExtra/gridExtra_0.9.tar.gz",
+#                  repos=NULL, type="source")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/scales/scales_0.2.4.tar.gz",
+#                  repos=NULL, type="source")
+# install.packages("gridExtra")
